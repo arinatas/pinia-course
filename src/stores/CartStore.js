fa94ce177a0ceaf2c15d98feaabcc379707beead
arinatas <97651013+arinatas@users.noreply.github.com>
2022-08-22 14:00:04 +0800

    done beginer pinia

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,7 @@
+import { isEmpty } from "lodash";
+import { groupBy } from "lodash";
 import { defineStore } from "pinia";
+import { useAuthUserStore } from "./AuthUserStore";
 
 export const useCartStore = defineStore("CartStore", {
     state: () => {
@@ -6,13 +9,48 @@ export const useCartStore = defineStore("CartStore", {
             items:[],
         }
     },
+    getters: {
+        // arrow function way underhere
+        count: (state) => state.items.length,
+        isEmpty: (state) => state.count == 0,
+        grouped: (state) => {
+            const grouped = groupBy(state.items, (item) => item.name);
+            const sorted = Object.keys(grouped).sort();
+            let in0rder = {};
+            sorted.forEach((key) => in0rder[key] = grouped[key]);
+            return in0rder;
+        },
+        groupCount: (state) => (name) => state.grouped[name].length,
+        total: (state) => state.items.reduce((p, c) => p + c.price, 0),
+
+        // normal function way under here
+        // count() {
+        //     return this.items.length;
+        // },
+        // isEmpty(){
+        //     return this.count == 0;
+        // }
+    },
 
     actions: {
+        checkout(){
+            const authUserStore = useAuthUserStore();
+            alert(
+                `${authUserStore.username} just bought ${this.count} items at a total of ${this.total}`
+            )
+        },
         addItems(count, item) {
             count = parseInt(count);
             for(let index = 0; index < count; index++){
                 this.items.push({...item})
               }
+        },
+        clearItem(itemName){
+            this.items = this.items.filter(item => item.name != itemName)
+        },
+        setItemCount(item, count){
+            this.clearItem(item.name);
+            this.addItems(count, item);
         }
     }
 });
